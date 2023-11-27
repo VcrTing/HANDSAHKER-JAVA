da3a7fb73dfed77e8a10973aabb791a8ebc27e0a
qiong <vcrting@163.com>
2023-11-27 20:06:37 +0800

    0

--- handshaker/src/main/java/com/qiong/handshaker/data/security/DataSecurityRoleConf.java
+++ handshaker/src/main/java/com/qiong/handshaker/data/security/DataSecurityRoleConf.java
@@ -1,5 +1,7 @@
 package com.qiong.handshaker.data.security;
 
+import java.util.Objects;
+
 public interface DataSecurityRoleConf {
 
     // ADMIN
@@ -15,4 +17,12 @@ public interface DataSecurityRoleConf {
     String AUTH_ADMIN_ONLY = "hasAuthority('" + ADMIN + "')";
     String AUTH_CASHIER = "hasAnyAuthority('" + CASHIER + "', '" + ADMIN + "')";
     String HAS_AUTH_ANONYMOUS = "hasAnyAuthority('" + ANONYMOUS + "', '" + CASHIER + "', '" + ADMIN + "')";
+
+    /**
+    * 工具方法
+    * @params
+    * @return
+    */
+    static boolean isAdmin(String src) { return Objects.equals(src, ADMIN); }
+    default boolean isAdminAuth(String src) { return Objects.equals(src, AUTH_ADMIN_ONLY); }
 }
